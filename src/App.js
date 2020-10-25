6a7e4e0c769bbe8ee16f3b39e53ae49ef43ef4c0
Alina Ilina <alinailina.random@gmail.com>
2020-10-25 16:10:39 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <iframe title="rentle" src="https://rentle.shop/alinailina"></iframe>
+    </div>
+  );
+}
+
+export default App;
